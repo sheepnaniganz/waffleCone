ebaf4790b6a9ad8ab862a86ab1c80bed215d075e
Sheepnaniganz <git.github@s.voldemail.com>
2023-08-23 10:15:25 -0400

    update upstream

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "sheepnaniganz/waffleCone:main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
